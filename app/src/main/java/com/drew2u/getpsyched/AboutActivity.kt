6b515fff0079d7716dc7a7cb2b14e7f9721244f7
kkoder32615 <kkoder32615@gmail.com>
2020-11-24 11:50:33 -0500

    Moved firstRunCheck to LauncherActivity.kt, just makes sense right?? Added some new-lines to xml to get rid of that ugly github warning.

--- app/src/main/java/com/drew2u/getpsyched/AboutActivity.kt
+++ app/src/main/java/com/drew2u/getpsyched/AboutActivity.kt
@@ -1,27 +1,16 @@
 package com.drew2u.getpsyched
 
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.edit
-import kotlin.system.exitProcess
-
-private const val FIRST_RUN = "firstRun"
 
 class AboutActivity : AppCompatActivity() {
-    private lateinit var sharedPrefs: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        sharedPrefs = getSharedPreferences("com.drew2u.getpsyched", Context.MODE_PRIVATE)
-        firstRunCheck(sharedPrefs)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -37,26 +26,5 @@ class AboutActivity : AppCompatActivity() {
             }
         }
         return true
-    }
-
-    /**
-     * Checks if the terms have been accepted or not. If the have not, the user is prompted to
-     * accept terms or exit the app.
-     */
-    private fun firstRunCheck(sp: SharedPreferences) {
-        if (sp.getBoolean(FIRST_RUN, true)) {
-            with(AlertDialog.Builder(this)) {
-                setTitle(getString(R.string.welcome))
-                setMessage(getString(R.string.first_run_message))
-                setPositiveButton(getString(R.string.agree), fun(_, _) {
-                    sp.edit { putBoolean(FIRST_RUN, false) }
-                })
-                setNegativeButton(getString(R.string.exit), fun(_, _) {
-                    exitProcess(0)
-                })
-                setCancelable(true)
-                show()
-            }
-        }
     }
 }
--- app/src/main/java/com/drew2u/getpsyched/LauncherActivity.kt
+++ app/src/main/java/com/drew2u/getpsyched/LauncherActivity.kt
@@ -1,20 +1,51 @@
 package com.drew2u.getpsyched
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import kotlin.system.exitProcess
+
+private const val FIRST_RUN = "firstRun"
 
 class LauncherActivity : AppCompatActivity() {
 
-    private lateinit var btnEnter: Button
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
 
+        sharedPreferences = getSharedPreferences("com.drew2u.getpsyched", Context.MODE_PRIVATE)
+        firstRunCheck(sharedPreferences)
+
         findViewById<Button>(R.id.btn_enter).setOnClickListener {
             startActivity(Intent(this, AboutActivity::class.java))
+        }
+    }
+
+    /**
+     * Checks if the terms have been accepted or not. If the have not, the user is prompted to
+     * accept terms or exit the app.
+     */
+    private fun firstRunCheck(sp: SharedPreferences) {
+        if (sp.getBoolean(FIRST_RUN, true)) {
+            with(AlertDialog.Builder(this)) {
+                setTitle(getString(R.string.welcome))
+                setMessage(getString(R.string.first_run_message))
+                setPositiveButton(getString(R.string.agree), fun(_, _) {
+                    sp.edit { putBoolean(FIRST_RUN, false) }
+                })
+                setNegativeButton(getString(R.string.exit), fun(_, _) {
+                    exitProcess(0)
+                })
+                setCancelable(true)
+                show()
+            }
         }
     }
 }
