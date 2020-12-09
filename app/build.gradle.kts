6f0fdfffcde51eabb0a3d869e22a0a55e4db7e3a
kkoder32615 <kkoder32615@gmail.com>
2020-12-09 17:17:04 -0500

    Updated: bulidtools 30.0.2 -> 30.0.3 kotlin 1.4.20 -> 1.4.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     compileSdkVersion(30)
-    buildToolsVersion = "30.0.2"
+    buildToolsVersion = "30.0.3"
 
     defaultConfig {
         applicationId = "com.drew2u.getpsyched"
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.21")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.2.1")
